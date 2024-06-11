9d45e072d5efacb3bbef0fd0e1bd0e9dc3448938
DylanVallejo <dylan1811vallejo@gmail.com>
2024-06-10 23:48:58 -0500

    creacion de componente bara de busqueda y listado de gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBarBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBarBoxComponent
+  ],
   imports: [
     CommonModule
+  ],exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
