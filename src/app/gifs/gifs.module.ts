21ba2879ee18e910b8675d4b8a3fcafa3f08028f
DylanVallejo <dylan1811vallejo@gmail.com>
2024-06-11 23:20:08 -0500

    creacion de la estructura card-list  y su componente

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBarBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBarBoxComponent
+    SearchBarBoxComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
   ],exports: [
-    HomePageComponent
+    HomePageComponent,
+    CardListComponent
   ]
 })
 export class GifsModule { }
