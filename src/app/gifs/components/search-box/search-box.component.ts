2956b1d9a6f5e093caa4942c0e9179a9a3a32026
DylanVallejo <dylan1811vallejo@gmail.com>
2024-06-30 22:44:07 -0500

    haciendo uso de ViewChild para tomar referencias locales de elementos

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs..">
+    <input type="text"
+    class="form-control"
+    placeholder="Buscar gifs.."
+    (keyup.enter)="searchTag( )"
+    #txtTagInput
+    >
 
   `
 })
 
 export class SearchBarBoxComponent {
+
+
+  // view chiod nos sirve para tomar referencias locales un valor
+  //  view children toma todos los inpust html arreglo
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor(){}
 
+  // searchTag(newTag : string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag})
+  }
 
 }
