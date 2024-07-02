10ad1efaaa17a5f0211ca4b9986f6d9a433fa218
DylanVallejo <dylan1811vallejo@gmail.com>
2024-07-01 23:31:24 -0500

    creacion de un servicio para manejar los gifs ingresados de manera global - consumo del servicio en sidebar para mostrar las busquedas realizadas

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -22,12 +23,14 @@ export class SearchBarBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor(){}
+  constructor( private gifsService: GifsService  ){}
 
   // searchTag(newTag : string) {
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({newTag})
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+    // console.log({newTag})
   }
 
 }
