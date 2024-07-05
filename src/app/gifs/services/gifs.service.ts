21ee5108f992f2daeb1444181c2822b114e8b288
DylanVallejo <dylan1811vallejo@gmail.com>
2024-07-04 23:58:41 -0500

    Importando e inyectando HttpClient para  el uso de metodos https  agregando params a la solicitud y mejorando la estructura de la solicitud

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
   private _tagsHistory: string [] = [];
+  private apiKey:          string = '0HL5FAzflVcb6j5DAKxknSXwhkCpU45z';
+  private serviceUrl:string       = 'https://api.giphy.com/v1/gifs';
 
-
-  private apiKey:string = '0HL5FAzflVcb6j5DAKxknSXwhkCpU45z';
-
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -33,15 +33,25 @@ export class GifsService {
   }
 
 
-  async searchTag( tag:string ):Promise<void> {
+  searchTag( tag:string ):void {
 
     if(tag.length === 0) return;
 
     this.organizeHistory(tag);
 
-    const resp = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=0HL5FAzflVcb6j5DAKxknSXwhkCpU45z&q=dota&limit=5')
-    const data = resp.json();
-    console.log(data)
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '5')
+    .set('q', tag)
+
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+    .subscribe(  response => {
+
+      console.log(response)
+      
+    })
 
   }
 
