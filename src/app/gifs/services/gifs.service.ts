2d0215441f9238b51859c3e5226b9b6c7f0f9634
DylanVallejo <dylan1811vallejo@gmail.com>
2024-07-11 23:38:58 -0500

    Agregando  el alamcenamiento en el localstorage y la validacion de la existencia de datos para realizar un fetch del primer dato almacenado en el localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,9 @@ export class GifsService {
   private apiKey:          string = '0HL5FAzflVcb6j5DAKxknSXwhkCpU45z';
   private serviceUrl:string       = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage()
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -30,10 +32,24 @@ export class GifsService {
     this._tagsHistory.unshift( tag );
     // fijamos una cantidad de busquedas
     this._tagsHistory= this.tagsHistory.splice(0,10);
-
+    //guardadndo en localstorage
+    this.saveLocalStorage();
 
   }
 
+  private saveLocalStorage():void {
+    localStorage.setItem("storageHistory" , JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if(!localStorage.getItem("storageHistory")) return;
+
+    // debemos usar el not null assertion
+    this._tagsHistory =  JSON.parse(localStorage.getItem("storageHistory")!);
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag( this._tagsHistory[0] );
+
+  }
 
   searchTag( tag:string ):void {
 
@@ -52,7 +68,7 @@ export class GifsService {
     .subscribe(  response => {
 
       this.gifsList = response.data;
-  
+
     })
 
   }
